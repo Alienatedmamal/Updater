9b84d10aedb9d6fa3dc5dd8dab14dffc57cb510f
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-15 14:11:14 -0600

    Update update.sh

--- Update/update.sh
+++ Update/update.sh
@@ -3,5 +3,6 @@ DIR="$(cd "$(dirname "$0")"/../../../../ && pwd)"
 source "$DIR/config.sh"
 
 
-echo '$TESTING="$SAY this is a test"' >> $DIR/AMAP/config.sh
-cp menu $DIR/AMAP/Files/Images/menu
+echo "Updating Packages...."
+sleep 2 
+cp menu $DIR/AMAP/Files/Images/
