ed7dfe0a3f66c0c5a2e9e3d9bd068cec91b76b2c
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-15 16:01:13 -0600

    Update update.sh

--- Update/update.sh
+++ Update/update.sh
@@ -8,6 +8,8 @@ source "$DIR/AMAP/config.sh"
 
 echo "Updating Packages...."
 sleep 2
-#echo "Installing new Menu"
+echo "Installing new Menu"
+echo $DIR/AMAP/Files/Updater
+
 #cp -f $DIR/AMAP/Files/Updater/Update/menu $DIR/AMAP/Files/Images
 echo "Finished"
