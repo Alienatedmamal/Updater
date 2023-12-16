ca548adfc3535df074a21774299d3f578d0e7478
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-15 22:51:50 -0600

    Update update.sh

--- Update/update.sh
+++ Update/update.sh
@@ -4,8 +4,6 @@ source "$DIR/config.sh"
 
 echo "Updating Packages...."
 sleep 2
-echo "Version Change v2.0.6..." 
-sleep 3
 cp -f $DIR/Updater/Update/version.txt $DIR/Files/
 sleep 1
 echo "Updates have been installed"
