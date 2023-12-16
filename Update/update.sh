a9f6a49f12158b985997ae8a695c380a1d8b21b0
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-16 15:02:25 -0600

    v2.0.6.1

--- Update/update.sh
+++ Update/update.sh
@@ -5,12 +5,12 @@ source "$DIR/config.sh"
 echo "Updating Packages...."
 sleep 2
 cp -f $DIR/Updater/Update/version.txt $DIR/Files/
-#sleep 1
-#echo "Updater Patch...."
-#sleep 1
-#cp -f $DIR/Updater/Update/Updater.sh $DIR/Files/Scripts/
-#echo "Patch Installed" || echo "Patch failed to install"
-#sleep 2
+sleep 1
+echo "Updater Patch...."
+sleep 1
+cp -f $DIR/Updater/Update/Updater.sh $DIR/Files/Scripts/
+echo "Patch Installed" || echo "Patch failed to install"
+sleep 2
 echo "Updates have been installed"
 sleep 2
 cat $DIR/Updater/Update/Updates
