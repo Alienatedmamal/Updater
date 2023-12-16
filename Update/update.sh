a64736586ed6307e1551778fc9a57559afabdacb
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-15 19:15:54 -0600

    Update update.sh

--- Update/update.sh
+++ Update/update.sh
@@ -7,5 +7,10 @@ sleep 2
 echo "Installing new Menu"
 sleep 1
 cp -f $DIR/AMAP/Files/Updater/Update/menu $DIR/AMAP/Files/Images
-echo "Finished"
+echo "Adding new parameters"
+sleep 2
+echo 
+
+echo 
+cat $DIR/AMAP/Files/Updater/Update/AMAP.sh >> $DIR/AMAP/AMAP.sh
 sleep 2
