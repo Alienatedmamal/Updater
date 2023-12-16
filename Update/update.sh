85f801a2830233fe25792c0fdf5415bc1fc9c050
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-15 22:14:03 -0600

    Update update.sh

--- Update/update.sh
+++ Update/update.sh
@@ -4,20 +4,10 @@ source "$DIR/config.sh"
 
 echo "Updating Packages...."
 sleep 2
-echo "Updating Menu...."
-sleep 2
-cp -f $DIR/Updater/Update/menu $DIR/Files/Images/
-sleep 1
-echo "New Configuration Image...."
-sleep 2
-cp -f $DIR/Updater/Update/backup $DIR/Files/Images/
-sleep 1
 echo "Version Change v2.0.6..." 
 sleep 3
 cp -f $DIR/Updater/Update/version.txt $DIR/Files/
-echo "Logo Update...."
 sleep 1
-cp -f $DIR/Updater/Update/logo $DIR/Files/Images/
 echo "Updates have been installed"
 sleep 2
 cat $DIR/Updater/Update/Updates
