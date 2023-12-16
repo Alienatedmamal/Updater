4bac046536377bedac867f35a1f3b0a81447aee9
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-16 15:44:20 -0600

    Update update.sh

--- Update/update.sh
+++ Update/update.sh
@@ -2,16 +2,9 @@
 DIR="$(cd "$(dirname "$0")"/../../ && pwd)"
 source "$DIR/config.sh"
 
-echo "Updating Packages...."
+echo "Updating Version...."
 sleep 2
 cp -f $DIR/Updater/Update/version.txt $DIR/Files/
-sleep 1
-echo "Updater Patch...."
-sleep 1
-cp -f $DIR/Updater/Update/Updater.sh $DIR/Files/Scripts/
-echo "Patch Installed" || echo "Patch failed to install"
-sleep 2
-echo "Updates have been installed"
 sleep 2
 cat $DIR/Updater/Update/Updates
 sleep 5
