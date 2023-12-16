1adf40a7fcea202de3c1df435bf8467fd6532175
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-15 21:05:30 -0600

    Update update.sh

--- Update/update.sh
+++ Update/update.sh
@@ -7,7 +7,13 @@ sleep 2
 echo "Updating Menu...."
 sleep 2
 cp -f $DIR/Updater/Update/menu $DIR/Files/Images/
+sleep 1
+echo "New Configuration Image...."
+sleep 2
+cp -f $DIR/Updater/Update/backup $DIR/Files/Images/
+sleep 1
 echo "Updates have been installed"
+sleep 1 
 cat $DIR/Updater/Update/Updates
 sleep 5
 exit 0 
