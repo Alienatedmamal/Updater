7117238d4ef315845a0d1a513ed4f4cc4451a7c4
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-15 21:38:25 -0600

    Update update.sh

--- Update/update.sh
+++ Update/update.sh
@@ -2,7 +2,25 @@
 DIR="$(cd "$(dirname "$0")"/../../ && pwd)"
 source "$DIR/config.sh"
 
-echo "Updating Packages...."
+# Current Version 
+desired_version="2.0.6"
+
+# File to check version. 
+version_file="$DIR/Files/"
+
+# Check if the file exists
+if [ -e "$version_file" ]; then
+    # Read the version number from the file
+    current_version=$(cat "$version_file")
+
+    # Compare the current version with the desired version
+    if [ "$current_version" = "$desired_version" ]; then
+        echo "Version is already up to date. Stopping further actions."
+        exit 0  # Exit with success status
+    else
+        echo "New Version Available"
+		echo "Updating to latest version...."
+        echo "Updating Packages...."
 sleep 2
 echo "Updating Menu...."
 sleep 2
@@ -14,9 +32,33 @@ cp -f $DIR/Updater/Update/backup $DIR/Files/Images/
 sleep 1
 echo "Version Change v2.0.6..." 
 sleep 3
+cp -f $DIR/Updater/Update/version.txt $DIR/Files/
+echo "Logo Update...."
+sleep 1
 cp -f $DIR/Updater/Update/logo $DIR/Files/Images/
 echo "Updates have been installed"
 sleep 2
 cat $DIR/Updater/Update/Updates
 sleep 5
-exit 0 
+    fi
+else
+    sleep 2
+echo "Updating Menu...."
+sleep 2
+cp -f $DIR/Updater/Update/menu $DIR/Files/Images/
+sleep 1
+echo "New Configuration Image...."
+sleep 2
+cp -f $DIR/Updater/Update/backup $DIR/Files/Images/
+sleep 1
+echo "Version Change v2.0.6..." 
+sleep 3
+cp -f $DIR/Updater/Update/version.txt $DIR/Files/
+echo "Logo Update...."
+sleep 1
+cp -f $DIR/Updater/Update/logo $DIR/Files/Images/
+echo "Updates have been installed"
+sleep 2
+cat $DIR/Updater/Update/Updates
+sleep 5
+fi
