e77f117ec528f1029f1102369b15d6d1abd3b22d
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-17 17:02:36 -0600

    Update update.sh

--- Update/update.sh
+++ Update/update.sh
@@ -5,13 +5,18 @@ source "$DIR/config.sh"
 
 echo "Updater Is Now Running"
 sleep 2
+echo "Updating Wipe Configurator...."
+sleep 2 
+cp -f $DIR/Updater/Updater/wipeconfigure.sh $DIR/Files/Scripts/
+echo "Wipe Configurator Updated" || echo "WIpe Configurator Failed to Update"
+sleep 2
 echo "Updating Version File...."
 sleep 2
 cp -f $DIR/Updater/Update/version.txt $DIR/Files/
 echo "Version File Updated" || echo "Version File FAILD to Update"
 sleep 2
 cat $DIR/Updater/Update/Updates
-sleep 3
+sleep 2
 echo "Updater has finished"
 echo "Press ENTER to continue"
 read -r
