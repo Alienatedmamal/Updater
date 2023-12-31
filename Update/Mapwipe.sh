0dc85cd8ff9a72236ecc4b4255d3ea4819b69f4b
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-31 14:43:00 -0600

    Create Mapwipe.sh

---
+++ Update/Mapwipe.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+DIR="$(cd "$(dirname "$0")/../../" && pwd)"
+source "$DIR/config.sh"
+
+
+#Stops Server, Performs Map Wipe.
+        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server will now stop >> $LOGS
+$USER $SERVER stop >> $LOGS
+        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server Has Stopped >> $LOGS ||
+        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server Has Failed to Stop >> $LOGS
+        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server Map will now wipe  >> $LOGS
+rm -fr /home/$USERNAME/serverfiles/server/$HOSTNAME/proceduralmap* >> $LOGS
+        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Map wipe has been completed.  >> $LOGS ||
+        $SAYDATE $SCRIPTMAPWIPE $SERVERNAMEMap has FAILED to Wipe  >> $LOGS
+        sleep 1
+        $SAYDATE $SCRIPTMAPWIPE Automation Map wipe completed  >> $LOGS
+        $SAYDATE $SCRIPTMAPWIPE Starting Server Now.....  >> $LOGS
