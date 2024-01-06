2b3affb85903bd18a48aab7746e5f087bbafd34e
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2024-01-06 16:24:53 -0600

    Update wipeconfigure.sh

--- Update/wipeconfigure.sh
+++ Update/wipeconfigure.sh
@@ -28,11 +28,12 @@ OUTPUT_FILE="$WIPER"
 
 # Generate output based on the wipe type
 if [ "$WIPE_TYPE" == "Full" ]; then
-   WIPE_COMMAND="commands[\"20$OYR-$OMN-$ODY\"]=\"/home/$USERNAME/AMAP/Files/Scripts/./Fullwipe.sh && sed -i 's/$OS/$NS/g' /home/$USERNAME/lgsm/config-lgsm/rustserver/common.cfg ; sed -i 's/$OS/$NS/g' /home/$USERNAME/lgsm/config-lgsm/rustserver/rustserver.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/$USERNAME/lgsm/config-lgsm/rustserver/common.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/$USERNAME/lgsm/config-lgsm/rustserver/rustserver.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/$USERNAME/serverfiles/oxide/config/ServerInfo.json ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/$USERNAME/serverfiles/oxide/config/SmartChatBot.json ; echo Files have been copied >> $LOGS || echo Failed to copy files >> $LOGS && /home/$USERNAME/ServerStart.sh" >> "$OUTPUT_FILE"
+   WIPE_COMMAND="commands[\"20$OYR-$OMN-$ODY\"]=\"/home/$USERNAME/AMAP/Files/Scripts/./Fullwipe.sh && sed -i 's/$OS/$NS/g' /home/$USERNAME/lgsm/config-lgsm/rustserver/common.cfg ; sed -i 's/$OS/$NS/g' /home/$USERNAME/lgsm/config-lgsm/rustserver/rustserver.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/$USERNAME/lgsm/config-lgsm/rustserver/common.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/$USERNAME/lgsm/config-lgsm/rustserver/rustserver.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/$USERNAME/serverfiles/oxide/config/ServerInfo.json ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/$USERNAME/serverfiles/oxide/config/SmartChatBot.json ; echo Files have been copied >> $LOGS || echo Failed to copy files >> $LOGS && /home/$USERNAME/AMAP/Files/Scripts/./ServerStart.sh\""
 echo "$WIPE_COMMAND" >> "$OUTPUT_FILE"
 elif [ "$WIPE_TYPE" == "Map" ]; then
-    WIPE_COMMAND="commands[\"20$OYR-$OMN-$ODY\"]=\"/home/$USERNAME/AMAP/Files/Scripts/./Mapwipe.sh && sed -i 's/$OS/$NS/g' /home/$USERNAME/lgsm/config-lgsm/rustserver/common.cfg ; sed -i 's/$OS/$NS/g' /home/$USERNAME/lgsm/config-lgsm/rustserver/rustserver.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/$USERNAME/lgsm/config-lgsm/rustserver/common.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/$USERNAME/lgsm/config-lgsm/rustserver/rustserver.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/$USERNAME/serverfiles/oxide/config/ServerInfo.json ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/$USERNAME/serverfiles/oxide/config/SmartChatBot.json ; echo Files have been copied >> $LOGS || echo Failed to copy files >> $LOGS && /home/$USERNAME/ServerStart.sh" >> "$OUTPUT_FILE"
- echo "$WIPE_COMMAND" >> "$OUTPUT_FILE"
+    WIPE_COMMAND="commands[\"20$OYR-$OMN-$ODY\"]=\"/home/$USERNAME/AMAP/Files/Scripts/./Mapwipe.sh && sed -i 's/$OS/$NS/g' /home/$USERNAME/lgsm/config-lgsm/rustserver/common.cfg ; sed -i 's/$OS/$NS/g' /home/$USERNAME/lgsm/config-lgsm/rustserver/rustserver.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/$USERNAME/lgsm/config-lgsm/rustserver/common.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/$USERNAME/lgsm/config-lgsm/rustserver/rustserver.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/$USERNAME/serverfiles/oxide/config/ServerInfo.json ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/$USERNAME/serverfiles/oxide/config/SmartChatBot.json ; echo Files have been copied >> $LOGS || echo Failed to copy files >> $LOGS && /home/$USERNAME/AMAP/Files/Scripts/./ServerStart.sh\""
+echo "$WIPE_COMMAND" >> "$OUTPUT_FILE"
+
 else
     echo "Invalid wipe type. Please enter 'Full' or 'Map'."
     exit 1
@@ -40,5 +41,3 @@ fi
 
 # Provide feedback to the user
 echo "Wipe details have been written to $OUTPUT_FILE"
-
-
