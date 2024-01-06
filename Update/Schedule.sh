8dc100a7fd78075253613bb89399d58ecc1f9766
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2024-01-06 08:20:43 -0600

    Create Schedule.sh

---
+++ Update/Schedule.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+DIR="$(cd "$(dirname "$0")/../../" && pwd)"
+source "$DIR/config.sh"
+
+# Get Todays Date. YYYY-MM-DD
+current_date=$(date +"%Y-%m-%d")
+
+# Run the Wipe Configurator in AMAP. It will create a file for this to read.
+# commands YR-MON-Day["TARGET-DATE-YOU-WANT"]="THE COMMAND YOU WANT TO RUN"
+
+declare -A commands
+
+# This is the output of Wipe Configurator
+source "$DIR/Files/Logs/WipeOutput.txt"
+
+
+
+# Check if the current date matches any of the target dates
+for target_date in "${!commands[@]}"; do
+    if [ "$current_date" == "$target_date" ]; then
+        # Run the corresponding command for the matched date
+        echo "Today's date matches a target date ($target_date). Running the command..." >> $LOGS
+        eval "${commands[$target_date]}"
+        exit 0
+    fi
+done
+
+# If no matches
+echo "$(date)" "There is no wipe this week." >> $LOGS
