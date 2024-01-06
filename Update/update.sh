647c38ad5de02b1f8fc79b4c8782b7bb952bab16
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2024-01-06 16:25:20 -0600

    Update update.sh

--- Update/update.sh
+++ Update/update.sh
@@ -5,11 +5,11 @@ source "$DIR/config.sh"
 
 echo "Updater Is Now Running"
 sleep 2
-#echo "Updating Wipeconfigure.sh...."
-#sleep 1 
-#cp -f $DIR/Updater/Update/wipeconfigure.sh $DIR/Files/Scripts/
-#echo "Wipeconfigure Updated"
-#sleep 2
+echo "Updating Wipeconfigure.sh...."
+sleep 1 
+cp -f $DIR/Updater/Update/wipeconfigure.sh $DIR/Files/Scripts/
+echo "Wipeconfigure Updated"
+sleep 2
 echo "Updating Schedule.sh...."
 cp -f $DIR/Updater/Update/Schedule.sh $DIR/Files/Scripts/
 echo "Schedule Updated"
