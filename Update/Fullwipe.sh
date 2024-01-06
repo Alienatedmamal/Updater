e0db3011bf2ebe934db9ff2effc6b71f6e4c0833
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2024-01-06 09:57:34 -0600

    Update Fullwipe.sh

--- Update/Fullwipe.sh
+++ Update/Fullwipe.sh
@@ -6,9 +6,12 @@ source "$DIR/config.sh"
 #Stops Server, Updates, Updates Mods, Performs Full Wipe.  
 	$SAYDATE $SCRIPTFULL $SERVERNAME Server will now stop.... >> $LOGS
 $USER $SERVER stop >> $LOGS
-$USER $SERVER update >> $LOGS
 	$SAYDATE $SCRIPTFULL $SERVERNAME Server Has Stopped >> $LOGS ||
         $SAYDATE $SCRIPTFULL $SERVERNAME Server Has Failed to Stop >> $LOGS
+	$SAYDATE $SCRIPTFULL $SERVERNAME Server will now update.... >> $LOGS
+ $USER $SERVER update >> $LOGS
+ $SAYDATE $SCRIPTFULL $SERVERNAME Server has updated >> $LOGS || 
+ $SAYDATE $SCRIPTFULL $SERVERNAME Server has failed to update >> $LOGS
         $SAYDATE $SCRIPTFULL $SERVERNAME Server will now begin full wipe.... >> $LOGS
 	$SAYDATE $SCRIPTFULL $SERVERNAME Removing Players Data..... >> $LOGS
 rm -fr /home/$USERNAME/serverfiles/server/$HOSTNAME/player* >> $LOGS
