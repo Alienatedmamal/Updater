ac00b8edb7a9535ea6dc809b3bae0f6d484d3535
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2024-01-06 08:26:19 -0600

    Update Schedule.sh

--- Update/Schedule.sh
+++ Update/Schedule.sh
@@ -19,11 +19,11 @@ source "$DIR/Files/Logs/WipeOutput.txt"
 for target_date in "${!commands[@]}"; do
     if [ "$current_date" == "$target_date" ]; then
         # Run the corresponding command for the matched date
-        echo "Today's date matches a target date ($target_date). Running the command..." >> $LOGS
+        echo "$DATE Schedule: Today's date matches a target date ($target_date). Running the command..." >> $LOGS
         eval "${commands[$target_date]}"
         exit 0
     fi
 done
 
 # If no matches
-echo "$(date)" "There is no wipe this week." >> $LOGS
+echo "$DATE Schedule: There is no wipe this week." >> $LOGS
