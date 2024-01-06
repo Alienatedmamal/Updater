5dc67729492053ed924f72da397c6e398eb71c63
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2024-01-06 08:10:25 -0600

    Update Fullwipe.sh
    - Fixed incorrect script tag to logs. Was $SCRIPTMAPWIPE and should of been $SCRIPTFULL. 
    - Added removing of player data to logs
    - Removed the need to remove map data for full wipe. 
    - Edit log outputs

--- Update/Fullwipe.sh
+++ Update/Fullwipe.sh
@@ -3,13 +3,16 @@ DIR="$(cd "$(dirname "$0")/../../" && pwd)"
 source "$DIR/config.sh"
 
 
-#Stops Server, Updates, Updates Mods, Performs Full Wipe.
-        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server will now stop >> $LOGS
+#Stops Server, Updates, Updates Mods, Performs Full Wipe.  
+	$SAYDATE $SCRIPTFULL $SERVERNAME Server will now stop.... >> $LOGS
 $USER $SERVER stop >> $LOGS
-        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server Has Stopped >> $LOGS ||
-        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server Has Failed to Stop >> $LOGS
-        $SAYDATE $SCRIPTFULL $SERVERNAME Server will now Full wipe >> $LOGS
+	$SAYDATE $SCRIPTFULL $SERVERNAME Server Has Stopped >> $LOGS ||
+        $SAYDATE $SCRIPTFULL $SERVERNAME Server Has Failed to Stop >> $LOGS
+        $SAYDATE $SCRIPTFULL $SERVERNAME Server will now begin full wipe.... >> $LOGS
+	$SAYDATE $SCRIPTFULL $SERVERNAME Removing Players Data..... >> $LOGS
 rm -fr /home/$USERNAME/serverfiles/server/$HOSTNAME/player* >> $LOGS
-rm -fr /home/$USERNAME/serverfiles/server/$HOSTNAME/proceduralmap* >> $LOGS
-        $SAYDATE $SCRIPTFULL $SERVERNAME Server Has Been Full-wiped>> $LOGS ||
-        $SAYDATE $SCRIPTFULL $SERVERNAME Full-wipe Has Failed to Wipe
+	$SAYDATE $SCRIPTFULL $SERVERNAME Player Data has been removed >> $LOGS
+	$SAYDATE $SCRIPTFULL $SERVERNAME Map data does not need to be removed >> $LOGS
+  	$SAYDATE $SCRIPTFULL $SERVERNAME Server Has Been Full-wiped>> $LOGS || 
+	$SAYDATE $SCRIPTFULL $SERVERNAME Full-wipe Has Failed to Wipe >> $LOGS  
+ 
