7e6716e59db65df8b16b62ff370f9bf6ab668d01
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2024-01-06 08:16:38 -0600

    Update Mapwipe.sh

--- Update/Mapwipe.sh
+++ Update/Mapwipe.sh
@@ -4,12 +4,14 @@ source "$DIR/config.sh"
 
 
 #Stops Server, Performs Map Wipe.
-        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server will now stop >> $LOGS
+        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server will now stop.... >> $LOGS
 $USER $SERVER stop >> $LOGS
         $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server Has Stopped >> $LOGS ||
         $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server Has Failed to Stop >> $LOGS
-        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server Map will now wipe  >> $LOGS
+        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server will now begin map wipe....  >> $LOGS
+        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Removing Map Data..... >> $LOGS
 rm -fr /home/$USERNAME/serverfiles/server/$HOSTNAME/proceduralmap* >> $LOGS
+        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Player Data has been removed >> $LOGS
         $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Map wipe has been completed.  >> $LOGS ||
         $SAYDATE $SCRIPTMAPWIPE $SERVERNAMEMap has FAILED to Wipe  >> $LOGS
         sleep 1
