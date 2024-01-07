d26c0a20bf11301eef9b01a560cef94f9e54a2b5
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2024-01-07 10:55:11 -0600

    Update update.sh

--- Update/update.sh
+++ Update/update.sh
@@ -5,42 +5,49 @@ source "$DIR/config.sh"
 
 echo "Updater Is Now Running"
 sleep 2
+echo "Updating ReadMe.md...."
+sleep 1
+cp -f $DIR/Updater/Update/ReadMe.md $DIR
+echo "ReadMe Updated"
+sleep 1
 echo "Updating Wipeconfigure.sh...."
 sleep 1 
 cp -f $DIR/Updater/Update/wipeconfigure.sh $DIR/Files/Scripts/
 echo "Wipeconfigure Updated"
-sleep 2
+sleep 1
 echo "Updating Schedule.sh...."
+sleep 1 
 cp -f $DIR/Updater/Update/Schedule.sh $DIR/Files/Scripts/
 echo "Schedule Updated"
-sleep 2
+sleep 1
 echo "Updating Fullwipe.sh...."
 sleep 1 
 cp -f $DIR/Updater/Update/Fullwipe.sh $DIR/Files/Scripts/
 echo "Fullwipe Updated"
-sleep 2 
+sleep 1 
 echo "Updating Mapwipe.sh...."
 sleep 1 
 cp -f $DIR/Updater/Update/Mapwipe.sh $DIR/Files/Scripts/
 echo "Mapwipe Updated"
-sleep 2 
+sleep 1 
 echo "Updating AMAP Dashboard...."
 sleep 1 
 cp -f $DIR/Updater/Update/AMAP.sh $DIR/
 cp -f $DIR/Updater/Update/AMAPNC.sh $DIR/
 echo "AMAP Dashboard Updated"
+sleep 1 
 echo "Updating Logo...."
 sleep 1 
 cp -f $DIR/Updater/Update/logo $DIR/Files/Images/
 echo "Logo Updated" 
-sleep 2
+sleep 1
 echo "Updating Version File...."
-sleep 2
+sleep 1
 cp -f $DIR/Updater/Update/version.txt $DIR/Files/
 echo "Version File Updated" 
-sleep 2
+sleep 1
 cat $DIR/Updater/Update/Updates
-sleep 2
+sleep 1
 echo "Updater has finished"
 echo "Press ENTER to continue"
 read -r
